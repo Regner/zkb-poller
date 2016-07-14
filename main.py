e1aa26cdd9d7c78385410ddbcc1a5299a8b9ba7e
Regner Blok-Andersen <shadowdf@gmail.com>
2016-07-14 12:00:48 +0300

    Publishing to NATS queue now

--- main.py
+++ main.py
@@ -17,29 +17,26 @@ logger.setLevel(logging.INFO)
 # App Settings
 ZKILLBOARD_REDISQ = 'http://redisq.zkillboard.com/listen.php'
 NATS_SERVERS = os.environ.get('NATS_SERVERS', 'nats://127.0.0.1:4222')
-# NATS_SERVERS = os.environ.get('NATS_SERVERS', 'nats://192.168.99.100:31356')
-
 
 
 async def run(loop):
-  client = Client()
-  servers = NATS_SERVERS.split(',')
+    client = Client()
+    servers = NATS_SERVERS.split(',')
 
-  await client.connect(io_loop=loop, servers=servers)
+    await client.connect(io_loop=loop, servers=servers)
+    logger.info('Connected to NATS server...')
 
-  while True:
-    with aiohttp.ClientSession() as session:
-        async with session.get(ZKILLBOARD_REDISQ) as resp:
-            data = await resp.json()
+    while True:
+        with aiohttp.ClientSession() as session:
+            async with session.get(ZKILLBOARD_REDISQ) as resp:
+                data = await resp.json()
 
-        if data['package'] is not None:
-            killmail = data['package']
+            if data['package'] is not None:
+                killmail = data['package']
 
-            logger.info('Publishing killmail with ID {}'.format(killmail['killID']))
+                logger.info('Publishing killmail with ID {}'.format(killmail['killID']))
 
-            await client.publish('zkillboard.raw', str.encode(json.dumps(killmail)))
-
-
+                await client.publish('zkillboard.raw', str.encode(json.dumps(killmail)))
 
 if __name__ == '__main__':
   loop = asyncio.get_event_loop()
